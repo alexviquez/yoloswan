16ce9cc5378964621a96630eaebb5efe7f3bf928
Alejandro <alexviquez22@gmail.com>
2015-10-10 08:29:30 -0500

    TheFirstCommit
    Come on !!

---
+++ Yoloswan/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Yoloswan
+//
+//  Created by Alejandro Viquez on 10/10/15.
+//  Copyright © 2015 Alejandro Viquez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("holis a todos")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
