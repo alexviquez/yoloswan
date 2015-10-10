b2bd178cc664bb2c519badc2af73bdaef35eaccc
Alejandro <alexviquez22@gmail.com>
2015-10-10 10:23:47 -0500

    holis
    aewf

--- Yoloswan/ViewController.swift
+++ Yoloswan/ViewController.swift
@@ -13,8 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print("holis a todos")
-        print("jojo")
+        print("awefaewf")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
